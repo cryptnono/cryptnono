7397c3b5f02c5ecffc07d82ca107dba4963c70b1
Simon Li <orpheus+devel@gmail.com>
2024-10-30 13:02:37 +0000

    monero.py: explain header search

--- scripts/monero.py
+++ scripts/monero.py
@@ -5,6 +5,8 @@
 from pathlib import Path
 from os import execl
 
+# Check for the presence of a linux header only present on newer kernels e.g.
+# /usr/src/linux-generic-6.1.30-0-virt/arch/x86/include/asm/fpu/api.h
 fpu_api = list(Path("/").glob("usr/src/*/arch/*/include/asm/fpu/api.h"))
 if len(fpu_api):
     v = "v2"
