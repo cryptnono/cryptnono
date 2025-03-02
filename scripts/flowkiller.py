6210ab861b5257cdda7642b0aa2366406c5b5cee
YuviPanda <yuvipanda@gmail.com>
2025-03-02 12:10:26 -0800

    Run pre-commit

--- scripts/flowkiller.py
+++ scripts/flowkiller.py
@@ -1,27 +1,35 @@
 #!/usr/bin/env python
 # Adapted from https://github.com/iovisor/bcc/blob/3f5e402bcadf44ce0250864db52673bf7317797b/tools/tcpconnect.py
 
-from functools import partial
-from pathlib import Path
-from bcc import BPF
 import argparse
-from ipaddress import ip_address, IPv4Address, IPv6Address
+from functools import partial
+from ipaddress import IPv4Address, IPv6Address, ip_address
+from pathlib import Path
 from struct import pack
 
-parser = argparse.ArgumentParser(
-    description="Kill processes based on tcp flows"
-)
+from bcc import BPF
+
+parser = argparse.ArgumentParser(description="Kill processes based on tcp flows")
 args = parser.parse_args()
 bpf_text = (Path(__file__).parent / "flowkiller.bpf.c").read_text()
 
-def handle_connection(pid: int, saddr: IPv4Address | IPv6Address, sport: int, daddr: IPv4Address | IPv6Address, dport: int):
+
+def handle_connection(
+    pid: int,
+    saddr: IPv4Address | IPv6Address,
+    sport: int,
+    daddr: IPv4Address | IPv6Address,
+    dport: int,
+):
     print([pid, saddr, sport, daddr, dport])
+
 
 def handle_event(event_name: str, b: BPF, cpu, data, size):
     event = b[event_name].event(data)
     saddr = ip_address(pack("I", event.saddr))
     daddr = ip_address(pack("I", event.daddr))
     handle_connection(event.pid, saddr, event.lport, daddr, event.dport)
+
 
 # initialize BPF
 b = BPF(text=bpf_text)
