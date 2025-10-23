aac424aa791e2bc284dbc57849507a19b6620b6f
Simon Li <orpheus+devel@gmail.com>
2025-10-23 21:22:46 +0100

    Test flowkiller metrics

--- tests/test_flowkiller_container.py
+++ tests/test_flowkiller_container.py
@@ -2,6 +2,19 @@ import os
 from subprocess import run
 
 import pytest
+import requests
+
+FLOWKILLER_METRICS_PORT = os.getenv("FLOWKILLER_METRICS_PORT", 12122)
+
+
+def get_metric(metric, default=0):
+    r = requests.get(f"http://localhost:{FLOWKILLER_METRICS_PORT}")
+    r.raise_for_status()
+    for line in r.text.splitlines():
+        m, value = line.split(" ", 1)
+        if m == metric:
+            return float(value)
+    return default
 
 
 def test_ipv4_allowed():
@@ -18,11 +31,18 @@ def test_ipv4_allowed():
     ],
 )
 def test_ipv4_killed(ip):
+    before = get_metric('cryptnono_flowkiller_processes_killed_total{source="ip"}')
+
     p = run(["curl", "--connect-timeout", "1", f"http://{ip}"])
     assert p.returncode == -9
 
+    after = get_metric('cryptnono_flowkiller_processes_killed_total{source="ip"}')
+    assert after > before
+
 
 def test_multiple_requests_killed():
+    before = get_metric('cryptnono_flowkiller_processes_killed_total{source="scan"}')
+
     p = run(
         [
             os.path.join(
@@ -33,3 +53,6 @@ def test_multiple_requests_killed():
         ]
     )
     assert p.returncode == -9
+
+    after = get_metric('cryptnono_flowkiller_processes_killed_total{source="scan"}')
+    assert after > before
